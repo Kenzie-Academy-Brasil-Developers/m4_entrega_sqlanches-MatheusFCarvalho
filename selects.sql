8e4c820ad399e305129d09a556503bb3aef7c352
Matheus Carvalho <matheus.fcarvalho2001@gmail.com>
2022-08-07 17:23:01 -0300

    feat: Selects

--- selects.sql
+++ selects.sql
@@ -3,10 +3,64 @@
 
 -- 1)
 
+SELECT *
+FROM produtos_pedidos pp
+JOIN produtos p ON p.id = pp.produto_id 
+JOIN pedidos pd ON pd.id = pp.pedido_id
+;
+
 -- 2)
 
+
+-- BUSCA SEM O ID DE FRITAS
+SELECT pedidos.id
+FROM produtos_pedidos 
+JOIN pedidos ON pedidos.id = produtos_pedidos.pedido_id
+JOIN produtos ON produtos.id = produtos_pedidos.produto_id
+WHERE produtos.nome LIKE 'Fritas'
+;
+
+
+-- BUSCA PELO ID DE FRITAS
+SELECT pedidos.id
+FROM produtos_pedidos 
+JOIN pedidos ON pedidos.id = produtos_pedidos.pedido_id
+WHERE produtos_pedidos.produto_id = 6;
+
+
 -- 3)
+-- BUSCA SEM O ID
+SELECT cl.nome gostam_de_fritas
+FROM pedidos ped
+JOIN clientes cl ON cl.id = ped.cliente_id
+JOIN produtos_pedidos pp ON pp.pedido_id = ped.id
+JOIN produtos p ON p.id = pp.produto_id
+WHERE p.nome = 'Fritas'
+;
+
+-- BUSCA PELO ID
+SELECT cl.nome gostam_de_fritas
+FROM pedidos ped
+JOIN clientes cl ON cl.id = ped.cliente_id
+JOIN produtos_pedidos pp ON pp.pedido_id = ped.id
+WHERE pp.produto_id = 6;
+;
 
 -- 4)
+SELECT SUM(preço)
+FROM produtos p
+JOIN produtos_pedidos pp ON pp.produto_id = p.id
+JOIN pedidos pd ON pd.id = pp.pedido_id
+JOIN clientes cl ON cl.id = pd.cliente_id
+WHERE cl.nome LIKE 'Laura'
+;
 
 -- 5)
+
+SELECT nome, COUNT(nome)
+FROM produtos p
+JOIN produtos_pedidos pp ON pp.produto_id = p.id
+GROUP BY nome
+;
+
+
